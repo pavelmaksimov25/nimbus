d75f6117c032664cc511defba7f4a2850ef76ef6
Pavlo Maksymov <maksimovpb@gmail.com>
2025-10-27 22:58:53 +0100

    feat: start task endpoint

--- internal/workflow/presenter/rest_api/task_handler.go
+++ internal/workflow/presenter/rest_api/task_handler.go
@@ -15,6 +15,7 @@ import (
 type Task struct {
 	ID        string `json:"id,omitempty"`
 	Payload   string `json:"payload"`
+	Status    string `json:"status,omitempty"`
 	CreatedAt string `json:"created_at,omitempty"`
 }
 
@@ -33,9 +34,7 @@ func (t *taskHandler) RegisterRoutes(rg *gin.RouterGroup) {
 	rg.POST("/tasks", t.handleCreateTask)
 	rg.GET("/tasks", t.handGetTasks)
 	rg.GET("/tasks/:id", t.handleGetTask)
-	rg.GET("/tasks/:id/status", func(c *gin.Context) {
-		c.JSON(http.StatusNotImplemented, gin.H{"message": "Nope"})
-	})
+	rg.POST("/tasks/:id/start", t.handleStartTask)
 }
 
 func (t *taskHandler) handleCreateTask(ctx *gin.Context) {
@@ -56,6 +55,7 @@ func (t *taskHandler) handleCreateTask(ctx *gin.Context) {
 
 	task.ID = taskEntity.ID.String()
 	task.CreatedAt = taskEntity.CreatedAt.Format(time.RFC3339)
+	task.Status = string(taskEntity.Status)
 
 	fmt.Printf("Received Task: %v", task)
 	ctx.JSON(http.StatusCreated, gin.H{"data": task})
@@ -81,4 +81,21 @@ func (t *taskHandler) handleGetTask(ctx *gin.Context) {
 		return
 	}
 	ctx.JSON(http.StatusOK, gin.H{"data": task})
+}
+
+func (t *taskHandler) handleStartTask(ctx *gin.Context) {
+	id, err := uuid.Parse(ctx.Param("id"))
+	if err != nil {
+		log.Printf("Invalid task ID: %s", err)
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Invalid task ID"})
+		return
+	}
+
+	err = t.service.StartTask(id)
+	if err != nil {
+		log.Printf("Error starting task: %s", err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Internal Server Error"})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "Task started"})
 }
